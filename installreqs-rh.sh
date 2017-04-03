aa71796cc331f927ddac4b4b610a474f4946c080
Louis Coilliot <louis.coilliot@gmail.com>
2017-04-03 21:42:14 +0200

    Typo

--- installreqs-rh.sh
+++ installreqs-rh.sh
@@ -1,3 +1,3 @@
 sudo yum -y install ack ctags git vim-common
 sudo yum -y install dejavu-fonts-common dejavu-sans-mono-fonts
-suod yum -y install python-flake8 # EPEL 7
+sudo yum -y install python-flake8 # EPEL 7
