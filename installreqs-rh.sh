ce3c1cdc2217b280fe9767d2e85ca5b5cbead661
Louis Coilliot <louis.coilliot@gmail.com>
2017-04-29 08:26:45 +0200

    Fix code block markdown syntax; better reqs

--- installreqs-rh.sh
+++ installreqs-rh.sh
@@ -1,3 +1,4 @@
-sudo yum -y install ack ctags git vim-common
-sudo yum -y install dejavu-fonts-common dejavu-sans-mono-fonts
-sudo yum -y install python-flake8 # EPEL 7
+sudo yum -y install ack ctags git vim-common \
+    dejavu-fonts-common dejavu-sans-mono-fonts \
+    libicu-devel \
+    python-flake8 # EPEL 7
--- readme.md
+++ readme.md
@@ -22,6 +22,7 @@ Tested on RHEL and Centos 7
 
 You need some recent packages for vim, flake8, dejavu fonts...
 
+```
 yum -y install ack ctags git vim-common
 yum -y install dejavu-fonts-common dejavu-sans-mono-fonts
 yum -y install python-flake8 # provided with EPEL 7
